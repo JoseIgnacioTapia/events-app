fcb06ba5208ef42ae52e7c98a9cb669c940d4290
JoseIgnacioTapia <josignaciot@gmail.com>
2021-11-16 21:27:00 -0300

    Adding Buttons & Icons

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,7 @@
-import Link from 'next/link';
-
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+import DateIcon from '../icons/date-icon';
+import Button from '../ui/button';
 import classes from './event-item.module.css';
 
 function EventItem(props) {
@@ -10,24 +12,31 @@ function EventItem(props) {
     month: 'long',
     year: 'numeric',
   });
-  const formattedAddress = location.replace(', ', '/n');
+  const formattedAddress = location.replace(', ', '\n');
   const exploreLink = `/events/${id}`;
 
   return (
     <li className={classes.item}>
       <img src={'/' + image} alt={title} />
-      <div className={classes.summary}>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
