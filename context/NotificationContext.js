7c837455cb7457f67fa2d32c75c14c90a7015653
JoseIgnacioTapia <josignaciot@gmail.com>
2021-12-06 19:56:41 -0300

    Creating a New React Context

---
+++ context/NotificationContext.js
@@ -0,0 +1,11 @@
+import { createContext, useState } from 'react';
+
+export const NotificationContext = createContext();
+
+const NotificationContextProvider = ({ children }) => {
+  return (
+    <NotificationContext.Provider>{children}</NotificationContext.Provider>
+  );
+};
+
+export default NotificationContextProvider;
